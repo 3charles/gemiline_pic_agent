6eefca55e22d04738f50fa96e2ecdb2c95259548
alanchen1115 <42991041+alanchen1115@users.noreply.github.com>
2026-08-14 15:11:35 +0800

    Update main.py

--- main.py
+++ main.py
@@ -151,7 +151,7 @@ def analyze_image_with_text(image_path: str, user_text: str) -> str:
         img_user = PIL.Image.open(image_path)
 
         response = client.models.generate_content(
-                        model="gemini-3.5-flash",
+                        model="gemini-3-flash-preview",
                         contents=[img_user, prompt],
                         config=generation_config
         )
